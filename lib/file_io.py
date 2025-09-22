d49c70c620ef310a10e6ffa7878af88856c250c7
Courtney Macgregor <cjoss90@hotmail.com>
2025-09-22 10:04:45 +1000

    Done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,20 @@
 def write_file(file_name, file_content):
-    pass
+    name = str(file_name)
+    if not name.endswith(".txt"):
+        name += ".txt"
+    with open(name, "w", encoding="utf-8") as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    name = str(file_name)
+    if not name.endswith(".txt"):
+        name += ".txt"
+    with open(name, "a", encoding="utf-8") as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    name = str(file_name)
+    if not name.endswith(".txt"):
+        name += ".txt"
+    with open(name, "r", encoding="utf-8") as f:
+        return f.read()
